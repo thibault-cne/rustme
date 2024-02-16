9b8f58fcbcef91dc4092681de52d23c6d29a4bb2
Thibault Cheneviere <thibault.chene23@gmail.com>
2024-02-16 11:45:19 -0500

    feat: added the google fonts to nano-font

--- bin/nano-fonts/src/main.rs
+++ bin/nano-fonts/src/main.rs
@@ -1,7 +1,12 @@
+use std::collections::HashMap;
+
 use base64::Engine;
 
 #[tokio::main]
 async fn main() {
+    // Load env variables from `.env` file
+    dotenvy::dotenv().unwrap();
+
     // Check if `json/` directory exists
     if !std::path::Path::new("json").exists() {
         std::fs::create_dir("json").unwrap();
@@ -13,11 +18,7 @@ async fn main() {
 
     // Convert all fonts to WOFF2 and save them to `json/` directory
     for font in FONTS {
-        let json_font = if font.remote_url.is_some() {
-            font.fetch().await.unwrap()
-        } else {
-            font.convert().unwrap()
-        };
+        let json_font = font.convert().unwrap();
 
         let json = serde_json::to_string(&json_font).unwrap();
 
@@ -27,12 +28,70 @@ async fn main() {
         )
         .unwrap();
     }
+
+    // Download all Google Fonts and save them to `json/` directory
+    for font in GoogleApiResponse::fetch()
+        .await
+        .unwrap()
+        .items
+        .iter()
+        .take(2)
+    {
+        if let Ok(json_font) = font.try_into_json().await {
+            let json = serde_json::to_string(&json_font).unwrap();
+
+            std::fs::write(
+                format!("json/{}.json", font.family.replace(' ', "_").to_lowercase()),
+                json,
+            )
+            .unwrap();
+        }
+    }
+}
+
+#[derive(serde::Deserialize)]
+struct GoogleApiResponse {
+    items: Vec<GoogleFont>,
+}
+
+impl GoogleApiResponse {
+    const BASE_URL: &'static str = "https://www.googleapis.com/webfonts/v1/webfonts?sort=alpha";
+
+    async fn fetch() -> Result<Self> {
+        let api_key = std::env::var("GOOGLE_API_KEY").unwrap();
+        let url = format!("{}&key={}", Self::BASE_URL, api_key);
+        let response = reqwest::get(url).await?.json().await?;
+        Ok(response)
+    }
+}
+
+#[derive(serde::Deserialize)]
+struct GoogleFont {
+    family: String,
+    files: HashMap<String, String>,
+}
+
+impl GoogleFont {
+    async fn try_into_json(&self) -> Result<JsonFont> {
+        if let Some(regular) = self.files.get("regular") {
+            let ttf = reqwest::get(regular).await?.bytes().await?;
+            let woff2 =
+                woff2::convert_ttf_to_woff2(&ttf, &[], 11, true).map_err(|_| Error::Woff2)?;
+            let base64 = base64::engine::general_purpose::STANDARD.encode(woff2);
+
+            Ok(JsonFont {
+                name: self.family.clone(),
+                base64: format!("data:font/woff2;charset=utf-8;base64,{}", base64),
+            })
+        } else {
+            Err(Error::RegularFontNotFound)
+        }
+    }
 }
 
 struct Font {
     name: &'static str,
     filename: &'static str,
-    remote_url: Option<&'static str>,
 }
 
 #[derive(serde::Deserialize, serde::Serialize)]
@@ -52,18 +111,11 @@ impl Font {
             base64: format!("data:font/woff2;charset=utf-8;base64,{}", base64),
         })
     }
-
-    async fn fetch(&self) -> Result<JsonFont> {
-        let url = format!("{}{}.json", self.remote_url.unwrap(), self.name);
-        let resp = reqwest::get(&url).await?;
-        resp.json().await.map_err(Error::Reqwest)
-    }
 }
 
 const FONTS: &[Font] = &[Font {
     name: "Formula 1",
     filename: "Formula1-Regular-1.ttf",
-    remote_url: None,
 }];
 
 type Result<T> = std::result::Result<T, Error>;
@@ -74,6 +126,7 @@ enum Error {
     Io(std::io::Error),
     Reqwest(reqwest::Error),
     Base64(base64::DecodeError),
+    RegularFontNotFound,
 }
 
 impl From<std::io::Error> for Error {
